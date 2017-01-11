9e4470b3b8ec2ed5ec1b43f01be38ec2d57966b8
Dalton Danis <daltond@shaw.ca>
2017-01-11 10:07:56 -0800

    Setup

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dalton
+ * Date: 2017-01-11
+ * Time: 10:01 AM
+ */
+
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+    <?php
+        echo "Hello out there... I live";
+    ?>
+    </body>
+</html>
